79c36cee5d05c1198a1ae97deb75f6357685e23e
swb <serhan@swb.sh>
2023-03-16 23:36:01 +0000

    release v1

--- ArpSpoofDetector.sh
+++ ArpSpoofDetector.sh
@@ -1,68 +1,84 @@
-#! /bin/bash
+#!/bin/bash
 
-#Introduction Section // git test
+# Function to print usage instructions
+print_usage() {
+    echo "Usage: $0 <interface_name>"
+    echo "Example: $0 eth0"
+}
 
-echo "Welcome to ArpSpoofDetector by Serhan Bahar"
+# Check if the script is run with root privileges
+if [ "$EUID" -ne 0 ]; then
+    echo "Please run this script as root or using sudo."
+    exit 1
+fi
 
+# Check if the required number of arguments is provided
+if [ "$#" -ne 1 ]; then
+    echo "Error: Invalid number of arguments."
+    print_usage
+    exit 1
+fi
+
+interface_name="$1"
+
+# Check if the provided interface is valid
+if ! ip link show "$interface_name" >/dev/null 2>&1; then
+    echo "Error: Invalid interface name."
+    print_usage
+    exit 1
+fi
+
+# Function to get MAC address
+get_mac_address() {
+    local ip="$1"
+    local iface="$2"
+    arp -a -i "$iface" | awk -v ip="$ip" '$2 ~ ip {print $4}'
+}
+
+# Introduction
+echo "Welcome to ArpSpoofDetector by Serhan W. Bahar"
 echo
 
-sleep 1
+# Find Router's IP Address
+routers_ip=$(route -n | awk '/UG/ {print $2}')
 
-echo -e "Please enter your interface name (like: eth0 or wlan0 etc.): \c "
+echo "Router's IP        : $routers_ip"
 
-read interface_name
+# Find Router's First MAC Address
+first_mac_ap=$(get_mac_address "$routers_ip" "$interface_name")
 
-echo
+echo "Router's First MAC : $first_mac_ap"
 
-#Find Router's IP Address
-routers_ip=$(route -n | awk '{print $2}' | grep -v IP | grep -v Gateway | grep -v 0.0.0.0)
-
-sleep 1
-
-echo "Router's IP		: $routers_ip"
-
-#Find Router's First MAC Address
-first_mac_ap=$(arp -a -i $interface_name | grep -e "gateway" -e "router" | awk '$2~/'"$routers_ip"'/ {print ($4)}')
-
-sleep 1
-
-echo "Router's First MAC	: $first_mac_ap"
-
-#Loop Timing
+# Loop Timing
 loop_max=500
-count=0
+detected=0
 
-while [[ $count -lt $loop_max ]];
-do
-	
-#Checking Router's MAC Address Again
+for ((count=0; count < loop_max; count++)); do
+    # Check Router's MAC Address Again
+    second_mac_ap=$(get_mac_address "$routers_ip" "$interface_name")
 
-    second_mac_ap=$(arp -a -i $interface_name | grep -e "gateway" -e "router" | awk '$2~/'"$routers_ip"'/ {print ($4)}')
-
-	    if [ "$second_mac_ap" != "$first_mac_ap" ];
-            then
-		    break
-	    fi
-
-	count=$(($count+1));
+    if [ "$second_mac_ap" != "$first_mac_ap" ]; then
+        detected=1
+        break
+    fi
 done
 
-sleep 1
+echo "Router's Second MAC : $second_mac_ap"
 
-echo "Router's Second MAC	:" $second_mac_ap
+detect_date=$(date)
+echo "Detect Date is      : $detect_date"
 
-sleep 1
-
-echo "Detect Date is		:" $(date)
-
-echo "Router's IP		: $routers_ip" > Results.txt && echo "Router's First MAC	: $first_mac_ap" >> Results.txt && echo "Router's Second MAC	:" $second_mac_ap >> Results.txt && echo "Detect Date is		:" $(date) >> Results.txt
-
-sleep 1
+# Save results to a file
+cat > Results.txt <<EOF
+Router's IP        : $routers_ip
+Router's First MAC : $first_mac_ap
+Router's Second MAC: $second_mac_ap
+Detect Date is     : $detect_date
+EOF
 
 echo
-
-echo "If the router's first MAC is different than second MAC please be careful about mitm attack, your file is ready: Results.txt"
-
-
-
-
+if [ $detected -eq 1 ]; then
+    echo "Warning: The router's first MAC is different than the second MAC. Please be careful about MITM attack. Check Results.txt for more information."
+else
+    echo "No ARP spoofing detected. Check Results.txt for more information."
+fi
--- README.md
+++ README.md
@@ -1,23 +1,46 @@
 # ArpSpoofDetector
 
-A bash script that detect ARP Spoofing or poisoning. If your network is attacked, this script will detect the attack and tell you the MAC address of attackers.
+ArpSpoofDetector is a simple bash script that helps you detect ARP spoofing attacks on your network. The script checks the MAC address of your router multiple times and compares it to detect any changes. If a change is detected, it could be a sign of a man-in-the-middle (MITM) attack.
 
-# Installation
+## Prerequisites
 
-Just Download the .sh file on your computer. (Tested on Ubuntu, Kali and Parrot OS)
+- Linux system with a Bash shell
+- Root or sudo privileges
 
-# How to run
+## Usage
 
-# Step 1:
+1. Clone this repository or download the `ArpSpoofDetector.sh` script to your local machine.
+2. Make the script executable using the following command:
 
-Open terminal where you download the file and run
-
+```
 chmod +x ArpSpoofDetector.sh
+```
 
-# Step 2:
+3. Run the script with root or sudo privileges and provide the interface name as an argument:
 
-Execute the script
+```
+sudo ./ArpSpoofDetector.sh <interface_name>
+```
 
-./ArpSpoofDetector.sh and enter the interface name.
+Replace `<interface_name>` with the name of your network interface, such as `eth0` or `wlan0`.
 
-Just that.
+Example:
+```
+sudo ./ArpSpoofDetector.sh eth0
+```
+
+The script will display the results in the terminal and save them to a `Results.txt` file. If the router's first MAC is different from the second MAC, be cautious about a possible MITM attack.
+
+## How It Works
+
+The script performs the following steps:
+
+1. Get the router's IP address from the routing table.
+2. Get the router's MAC address using the ARP table.
+3. Loop for a specified number of times (default: 500) and check the router's MAC address again.
+4. If the MAC address changes during the loop, the script detects a potential MITM attack.
+5. Display the results and save them to a Results.txt file.
+
+## Note
+
+This script is intended for educational and test purposes and should be used at your own risk. The detection of ARP spoofing attacks might not be perfect, and false positives or false negatives may occur.
